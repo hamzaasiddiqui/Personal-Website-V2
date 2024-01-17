e3ef1a4af8f2c210a5b6e5240445f907286368ff
hamzaasiddiqui <hamza.eins@gmail.com>
2024-01-17 18:17:49 +0400

    bug fixes and updates.

--- src/content_option.js
+++ src/content_option.js
@@ -36,14 +36,14 @@ const worktimeline = [
     date: "May 23 - present",
   },
   {
+    jobtitle: "Software Developer Intern",
+    where: "Keystone consulting",
+    date: "June 23 - Aug 23",
+  },
+  {
     jobtitle: "Sofware Engineering Intern",
     where: "Mantula Solar",
     date: "July 22 - Aug 22",
-  },
-  {
-    jobtitle: "Designer of week",
-    where: "ALquds",
-    date: "2019",
   },
 ];
 
@@ -66,16 +66,25 @@ const services = [
   {
     title: "Residential Network Administrator - NETRONiX (GIKI)",
     description: {
-      1: "- Manage and Administer GIK Institute’s mixed topological residential network consisting of over 3500 nodes",
-      2: "- Deliver tutorial sessions to junior members on computer networking topics including TCP/ IP, ARP requests routing fundamentals, resulting in an average score increase of 15%",
+      1: "- Manage GIK Institute’s residential network compromising of 3500+ nodes, ensuring robust performance.",
+      2: "- Designed and conducted comprehensive training sessions and workshops on computer networking for students.",
+      3: "- Provide and maintain peer-to-peer file sharing and IPTV services for 3000+ residents of GIK Institute.",
+    },
+  },
+  {
+    title: "Software Developer Intern - Keystone Consulting",
+    description: {
+      1: "- Led the development of an advanced business intelligence visualization application leveraging Natural Language Processing techniques, enhancing data interpretability and decision-making processes.",
+      2: "- Employed Flask, Python, React.js, and Next.js; contributing to a seamless UI and efficient back-end functionality.",
+      3: "- Integrated LangChain and OpenAI technologies, and authored multiple Python scripts to create a sophisticated chatbot interface, elevating user interaction and automating responses.",
     },
   },
   {
     title: "Software Engineering Intern - Mantula Solar",
     description: {
-      1: "- Collaborated with a team of five diverse individuals remotely, as a full-stack web developer, to develop Mantula’s web application using React.js and Firebase",
-      2: "- Optimized key components of the application resulting in a 60% increase in rendering speed and reduced server requests, contributing to improved user experience",
-      3: "- Implemented a decision tree price estimator in the UI components and connected APIs to meet customer preferences, providing a personalized experience for users",
+      1: "- Collaborated with a team of 5 full-stack web developers to engineer Mantula’s web application, utilizing React.js and Firebase, enhancing the product’s functionality and user interface.",
+      2: "- Developed and integrated a decision tree-based price estimator, along with connecting APIs to align with customer preferences, offering a tailored and engaging user experience.",
+      3: "- Leveraged various NPM packages, contributing to a sophisticated and seamless user experience and documented the software development lifecycle, paving the way for future enhancements and maintenance.",
     },
   },
 ];
@@ -127,7 +136,7 @@ const contactConfig = {
 const socialprofils = {
   linkedin: "https://www.linkedin.com/in/-hamza-siddiqui/",
   github: "https://github.com/hamzaasiddiqui",
-  resume: "https://drive.google.com/file/d/1KK6ZCAXuTALKAY55gt8opJKbs55J863A/view?usp=sharing",
+  resume: "https://drive.google.com/file/d/121oLDVB7UkkPDZpZ_2R-_4qkV6xpmTJs/view?usp=sharing",
 };
 
 const stacks = [
